093de7f2aa3f5ba45849a420474b70e3103aa52d
Sergey_Korotich <autodiller@bk.ru>
2022-10-26 01:40:18 +0500

    Finish213

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,17 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
 
+
+    private Long nanoTime = System.nanoTime();
+    @Autowired
+    @Qualifier("animalsCage")
     public Long getTime() {
         return nanoTime;
     }
